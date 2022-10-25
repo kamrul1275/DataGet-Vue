3a1364a93c188f0cf7b5b9b1ba0ba17f3dbab4c0
kamrul hassan <78846828+kamrul1275@users.noreply.github.com>
2022-10-25 22:35:02 +0600

    first commit

---
+++ src/components/Test.vue
@@ -0,0 +1,58 @@
+<template>
+     <div class="hello">
+       <h1>Post part.......</h1>
+       
+<div v-for="post in posts" :key="post.id">
+
+     <h2>{{ post.title}}</h2>
+<p>{{ post.body}}</p>
+
+</div>
+
+     
+     </div>
+   </template>
+   
+   <script>
+
+
+import axios from 'axios';
+
+   export default {
+     name: 'TestVue',
+
+     data(){
+
+return{
+
+posts:[],
+
+}
+
+},methods:{
+
+},
+
+
+created(){
+
+axios.get('https://jsonplaceholder.typicode.com/posts')
+.then(response=>{
+
+this.posts=response.data
+
+
+
+})
+.catch(error=>{
+     console.log(error);  
+});
+    
+}
+
+     }
+    
+   
+   </script>
+   
+ 
